d572c771cbc6fa7888c19538b8b3595259474cd6
liuxu <lxerxa@126.com>
2018-01-22 11:18:33 +0800

    add permissions in config page

--- app/Http/Controllers/ConfigController.php
+++ app/Http/Controllers/ConfigController.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\Controller;
 
 use DB;
 use App\Project\Provider;
+use App\Acl\Eloquent\RolePermissions;
 
 class ConfigController extends Controller
 {
@@ -33,7 +34,28 @@ class ConfigController extends Controller
 
         $priorities = Provider::getPriorityList($project_key);
         $roles = Provider::getRoleList($project_key);
+        foreach($roles as $role)
+        {
+            $role->permissions = $this->getPermissions($project_key, $role->id);
+        }
 
         return Response()->json([ 'ecode' => 0, 'data' => [ 'types' => $types, 'roles' => $roles, 'priorities' => $priorities ] ]);
+    }
+
+    /**
+     * get permissions by project_key and roleid.
+     *
+     * @param  string $project_key
+     * @param  string $role_id
+     * @return array
+     */
+    public function getPermissions($project_key, $role_id)
+    {
+        $rp = RolePermissions::where([ 'project_key' => $project_key, 'role_id' => $role_id ])->first();
+        if (!$rp && $project_key !== '$_sys_$')
+        {
+            $rp = RolePermissions::where([ 'project_key' => '$_sys_$', 'role_id' => $role_id ])->first();
+        }
+        return $rp && isset($rp->permissions) ? $rp->permissions : [];
     }
 }
