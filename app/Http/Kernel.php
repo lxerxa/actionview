870824d1b3286faa85b60ec2c1c9835bedcf0c03
lxerxa <lxerxa@126.com>
2019-01-16 22:51:47 +0800

    add before middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
+        \App\Http\Middleware\BeforeMiddleware::class,
         \App\Http\Middleware\AfterMiddleware::class
     ];
 
---
+++ app/Http/Middleware/BeforeMiddleware.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\JsonResponse;
+use Closure;
+
+class BeforeMiddleware 
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $params = $request->all();
+        foreach ($params as $k => $v)
+        {
+            if (is_string($v)) 
+            {
+                $request->offsetSet($k, trim($v));
+            }
+        }
+        return $next($request);
+    }
+}
