a4e0c1cacd84acfc119062f50bc97cb685d449e8
lxerxa <lxerxa@126.com>
2020-09-12 22:50:47 +0800

    pdf preview

--- app/Utils/File.php
+++ app/Utils/File.php
@@ -23,4 +23,23 @@ class File {
         fclose($fp);
         ob_end_clean();
     }
+
+    static function pdfPreview($filename, $displayname)
+    {
+        Header("Content-type: application/pdf");
+        header("Accept-Ranges: bytes");
+        header("Accept-Length:" . filesize($filename));
+
+        $fp = fopen($filename, 'rb');
+        ob_end_clean();
+        ob_start();
+        while(!feof($fp))
+        {
+            $chunk_size = 1024 * 8;
+            echo fread($fp, $chunk_size);
+            ob_flush();
+        }
+        fclose($fp);
+        ob_end_clean();
+    }
 }
