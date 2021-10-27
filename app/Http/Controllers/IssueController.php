161b6b377482308b2f691caf50ccb2bfd60f0f4c
lxerxa <lxerxa@126.com>
2021-10-27 20:11:35 +0800

    expect_complete_time

--- app/Http/Controllers/IssueController.php
+++ app/Http/Controllers/IssueController.php
@@ -911,7 +911,7 @@ class IssueController extends Controller
      */
     public function getValidKeysBySchema($schema=[])
     {
-        $valid_keys = array_merge(array_column($schema, 'key'), [ 'type', 'assignee', 'descriptions', 'labels', 'parent_id', 'resolution', 'priority', 'progress', 'expect_start_time', 'expect_compete_time' ]);
+        $valid_keys = array_merge(array_column($schema, 'key'), [ 'type', 'assignee', 'descriptions', 'labels', 'parent_id', 'resolution', 'priority', 'progress', 'expect_start_time', 'expect_complete_time' ]);
 
         foreach ($schema as $field)
         {
