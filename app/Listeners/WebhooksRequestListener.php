267f46a208ffda4b2b5aebb992e4944d24ab1f9b
lxerxa <lxerxa@126.com>
2022-09-03 19:10:52 +0800

    webhook

--- app/Listeners/WebhooksRequestListener.php
+++ app/Listeners/WebhooksRequestListener.php
@@ -1,9 +1,16 @@
 <?php
 namespace App\Listeners;
+
 use App\Events\Event;
 use App\Events\IssueEvent;
 use App\Events\VersionEvent;
 
+use App\Customization\Eloquent\State;
+use App\Customization\Eloquent\Resolution;
+use App\Customization\Eloquent\Priority;
+use App\Customization\Eloquent\Type;
+
+use App\Project\Provider;
 use App\Project\Eloquent\WebhookEvents;
 use App\Project\Eloquent\Webhooks;
 use App\Project\Eloquent\Version;
@@ -64,16 +71,149 @@ class WebhooksRequestListener
                 return;
             }
 
-            $data = DB::collection('issue_' . $project_key)->where('_id', $event->issue_id)->first();
-
-            $data['project_key'] = $project_key;
-            $data['event'] = $event_key;
-            unset($data['_id']);
-
+            $data = [];
             if ($event_key == 'add_worklog' || $event_key == 'edit_worklog')
             {
                 $data['worklog'] = $event->param['data'];
             }
+
+            $issue = DB::collection('issue_' . $project_key)->where('_id', $event->issue_id)->first();
+            if (!$issue)
+            {
+            	return;
+            }
+
+            $schema = Provider::getSchemaByType($issue['type']);
+            if (!$schema)
+            {
+            	return;
+            }
+
+            $new_issue = [];
+
+            $type = Type::find($issue['type']);
+            if (!$type)
+            {
+            	return;
+            }
+            $new_issue['type'] = $type->name;
+
+            foreach ($schema as $key => $field) 
+            {
+            	$field_key = $field['key'];
+            	if (!isset($issue[$field_key]))
+            	{
+	            continue;
+            	}
+
+            	$field_value = $issue[$field_key];
+            	if (!$field_value)
+            	{
+                    $new_issue[$field_key] = $field_value;
+            	    continue;
+            	}
+
+                if (isset($field['optionValues']) && $field['optionValues'])
+            	{
+                    $opv = [];
+                    
+                    if (!is_array($field_value))
+                    {
+                        $fieldValues = explode(',', $field_value);
+                    }
+                    else
+                    {
+                        $fieldValues = $field_value;
+                    }
+
+                    foreach ($field['optionValues'] as $ov)
+                    {
+                        if (in_array($ov['id'], $fieldValues))
+                        {
+                            $opv[] = $ov['name'];
+                        }
+                    }
+
+                    $new_issue[$field_key] = count($opv) == 1 && !is_array($field_value) && strpos($field_value, ',') === false ? $opv[0] : $opv;
+            	}
+            	else if ($field['type'] == 'DatePicker' || $field['type'] == 'DateTimePicker')
+            	{
+                    if ($field_value)
+            	    {
+                        $new_issue[$field_key] = date($field['type'] == 'DatePicker' ? 'Y-m-d' : 'Y-m-d H:i:s', $field_value);
+            	    }
+            	    else
+            	    {
+            	        $new_issue[$field_key] = $field_value;
+            	    }
+            	}
+            	else
+            	{
+            	    $new_issue[$field_key] = $field_value;
+            	}
+            }
+
+            if (isset($issue['state']) && !isset($new_issue['state']))
+            {
+            	$state = State::find($issue['state']);
+            	if ($state)
+            	{
+                    $new_issue['state'] = $state->name;
+            	}
+            }
+
+            if (isset($issue['resolution']) && !isset($new_issue['resolution']))
+            {
+            	$resolution = Resolution::find($issue['resolution']);
+            	if ($resolution)
+            	{
+                    $new_issue['resolution'] = $resolution->name;
+            	}
+            }
+
+            if (isset($issue['priority']) && !isset($new_issue['priority']))
+            {
+            	$priority = Priority::find($issue['priority']);
+            	if ($priority)
+            	{
+                    $new_issue['priority'] = $priority->name;
+            	}
+            }
+
+            if (isset($issue['descriptions']))
+            {
+            	$new_issue['descriptions'] = $issue['descriptions'];
+            }
+
+            if (isset($issue['labels']))
+            {
+            	$new_issue['labels'] = $issue['labels'];
+            }
+
+            if (isset($issue['assignee']))
+            {
+            	$new_issue['assignee'] = $issue['assignee'];
+            }
+
+            if (isset($issue['progress']))
+            {
+            	$new_issue['progress'] = $issue['progress'];
+            }
+
+            if (isset($issue['expect_start_time']))
+            {
+            	$new_issue['expect_start_time'] = $issue['expect_start_time'] ? date('Y-m-d', $issue['expect_start_time']) : '';
+            }
+
+            if (isset($issue['expect_complete_time']))
+            {
+            	$new_issue['expect_complete_time'] = $issue['expect_complete_time'] ? date('Y-m-d', $issue['expect_complete_time']) : '';
+            }
+
+            $data['issue'] = $new_issue;
+            $data['event'] = $event_key;
+            $data['project_key'] = $project_key;
+            $data['user'] = $user;
 
             $header = [ 'Content-Type: application/json', 'Expect:', 'X-Actionview-Token: ' . ($token ?: '') ];
             CurlRequest::post($request_url, $header, $data, 1);
@@ -85,10 +225,10 @@ class WebhooksRequestListener
                 return;
             }
 
-            $data = $event->param['data'];
-
+            $data['version'] = $event->param['data'];
             $data['event'] = $event_key;
-            unset($data['_id']);
+            $data['project_key'] = $project_key;
+            $data['user'] = $user;
 
             $header = [ 'Content-Type: application/json', 'Expect:', 'X-Actionview-Token: ' . ($token ?: '') ];
             CurlRequest::post($request_url, $header, $data, 1);
